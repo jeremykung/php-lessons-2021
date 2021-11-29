f116689ef1b4fae4cc6322667ea542dcbe75989d
sys72 <sys72@pop-os.localdomain>
2021-11-29 20:34:12 +0800

    updated lesson 3

--- lesson3/database.php
+++ lesson3/database.php
@@ -9,12 +9,31 @@ try {
     $conn = new PDO("mysql:host=localhost;dbname=message_board", 'root', 'lhmethod');
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    echo "Connected successfully<br>";
 
-    // WRITE TO DATABASE
-    $sql = "INSERT INTO `Message` (name, message) VALUES ('Timmy', 'I need to go washroom')";
-    $conn->exec($sql);
-    echo "Message added success!";
+    // READ
+
+    // $result = $conn->query("SELECT * FROM `Message`");
+    
+    $stmt = $conn->prepare("SELECT * FROM `Message`");
+    $stmt->execute();
+    
+    $result = $stmt->fetchAll();
+    var_dump($result);
+
+
+    foreach($result as $item) {
+        echo "<br>";
+        $name = $item["name"];
+        $message = $item["message"];
+        echo $name . "<br>";
+        echo $message . "<br>";
+    }
+
+    // WRITE
+    // $sql = "INSERT INTO `Message` (name, message) VALUES ('Timmy', 'I need to go washroom')";
+    // $conn->exec($sql);
+    // echo "Message added success!";
 
 } catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
